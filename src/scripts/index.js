36e2f77784acdf23e9bc0c90d90dd2da30802619
marten-mertan <46864964+marten-mertan@users.noreply.github.com>
2019-11-02 18:32:26 +0300

    Add files via upload

---
+++ src/scripts/index.js
@@ -0,0 +1,5 @@
+import initSliderMain from './parts/slider-main';
+
+window.onload = function() {
+    initSliderMain();
+};
---
+++ src/scripts/parts/slider-main.js
@@ -0,0 +1,14 @@
+function initSliderMain(){
+    $('.js-slider-main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        prevArrow: '<a class="arrows back"><img src="assets/img/index/back.svg" alt="arrow-left"></img></a>',
+        nextArrow: '<a class="arrows next"><img src="assets/img/index/next.svg" alt="arrow-right"></img></a>',
+    });
+}
+
+export default initSliderMain;
